7c1a836c0e41f5a7db8a2e223fbd2abc9cc7384e
masalmon <maelle.salmon@yahoo.se>
2016-05-20 17:32:18 +0200

    transforms the temporal coverage column

--- code.R
+++ code.R
@@ -1,10 +1,26 @@
 library("openxlsx")
 library("dplyr")
+library("tidyr")
 library("opencage")
 library("countrycode")
 file <- "data/WHO_AAP_database_May2016_v3web.xlsx"
 data <- read.xlsx(file, sheet = 2, startRow = 3)
 data <- tbl_df(data)
+
+data <- mutate(data,
+               countrycode = countrycode(iso3,"iso3c","iso2c"))
+data <- separate(data, temporal_coverage,
+                 c("PM10_temporal_coverage",
+                   "PM25_temporal_coverage"), sep =";")
+data <- mutate(data,
+               PM10_temporal_coverage = gsub("PM10:", "", PM10_temporal_coverage),
+               PM25_temporal_coverage = gsub("PM2\\.5:", "", PM25_temporal_coverage))
+
+data <- mutate(data, 
+               PM10_annually_rep = grepl("annually re", PM10_temporal_coverage), 
+               PM25_annually_rep = grepl("annually re", PM25_temporal_coverage))
+data <- mutate(data, 
+               PM10_more_75 = grepl(">75", PM10_temporal_coverage), 
+               PM25_more_75 = grepl(">75", PM25_temporal_coverage))
+
 View(data)
-data <- data %>% 
-  mutate(countrycode = countrycode(iso3,"iso3c","iso2c"))
