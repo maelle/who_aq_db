db7b7a947309feab151a6da725a23ee94715d7b7
masalmon <maelle.salmon@yahoo.se>
2016-05-20 16:58:09 +0200

    adds data

--- code.R
+++ code.R
@@ -1,7 +1,10 @@
 library("openxlsx")
 library("dplyr")
 library("opencage")
+library("countrycode")
 file <- "data/WHO_AAP_database_May2016_v3web.xlsx"
 data <- read.xlsx(file, sheet = 2, startRow = 3)
 data <- tbl_df(data)
 View(data)
+data <- data %>% 
+  mutate(countrycode = countrycode(iso3,"iso3c","iso2c"))
