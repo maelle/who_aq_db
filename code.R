c2e4fe120647e6317139b4fca132df91bb5d67c5
masalmon <maelle.salmon@yahoo.se>
2016-05-20 17:52:41 +0200

    updates code

--- code.R
+++ code.R
@@ -7,11 +7,19 @@ file <- "data/WHO_AAP_database_May2016_v3web.xlsx"
 data <- read.xlsx(file, sheet = 2, startRow = 3)
 data <- tbl_df(data)
 
+##################################################################
+# Transform country code
+##################################################################
+
 data <- mutate(data,
                countrycode = countrycode(iso3,"iso3c","iso2c"))
 data <- separate(data, temporal_coverage,
                  c("PM10_temporal_coverage",
                    "PM25_temporal_coverage"), sep =";")
+
+##################################################################
+# Transform temporal coverage
+##################################################################
 data <- mutate(data,
                PM10_temporal_coverage = gsub("PM10:", "", PM10_temporal_coverage),
                PM25_temporal_coverage = gsub("PM2\\.5:", "", PM25_temporal_coverage))
@@ -23,4 +31,16 @@ data <- mutate(data,
                PM10_more_75 = grepl(">75", PM10_temporal_coverage), 
                PM25_more_75 = grepl(">75", PM25_temporal_coverage))
 
-View(data)
+##################################################################
+# Transform stations information
+##################################################################
+# do by grepl
+# information not well organized
+sum(grepl(" urban", data$PM25_stations))
+sum(grepl("pre-urban", data$PM25_stations))
+# suburban
+# industrial
+# traffic
+# residential
+# commercial
+# rural
